db8eebda8c5a52a06792eeafa99c6a613c3e8098
Bombo <bombo96@chungwoon.ac.kr>
2023-03-10 15:26:30 +0900

    [전략패턴 V1 테스트]

---
+++ src/test/java/strategy/V1Test.java
@@ -0,0 +1,29 @@
+package strategy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.logging.LoggerFactory;
+import strategy.V1.Duck;
+import strategy.V1.MallardDuck;
+import strategy.V1.RedheadDuck;
+
+import java.util.logging.Logger;
+
+
+public class V1Test {
+
+    Logger log = (Logger) LoggerFactory.getLogger(V1Test.class);
+
+    @Test
+    void V1() throws Exception {
+        //given
+        MallardDuck mallardDuck = new MallardDuck();
+        RedheadDuck redheadDuck = new RedheadDuck();
+        //when
+
+        //then
+        mallardDuck.display();
+        redheadDuck.display();
+    }
+
+}
